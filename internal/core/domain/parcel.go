174f0483f14d00cef868cce4bbfedde79e474b78
Rens-Br <rensbrekel@gmail.com>
2022-04-13 09:45:29 +0200

    Update README to reflect changes

--- internal/core/domain/parcel.go
+++ internal/core/domain/parcel.go
@@ -4,9 +4,10 @@ type Parcel struct {
 	ID        string
 	Name      string
 	Size      Dimensions
+	Weight    int
 	DeliverId string
 }
 
-func NewParcel(id string, name string, size Dimensions) Parcel {
-	return Parcel{ID: id, Name: name, Size: size}
+func NewParcel(id string, name string, size Dimensions, weight int) Parcel {
+	return Parcel{ID: id, Name: name, Size: size, Weight: weight}
 }
